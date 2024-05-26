8ca36187ed18e6227b03e0fee3320f1e661b0e98
Ricardo Ferreira <141281476+ricardoof@users.noreply.github.com>
2024-05-26 10:51:04 -0300

    correção deploy

--- server.js
+++ server.js
@@ -15,9 +15,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Configurar tipos MIME para arquivos CSS
+app.use('/src/styles', express.static(path.join(__dirname, 'src/styles')));
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
-
-
