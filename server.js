9671dfdba8a57fee5867bfd735c29943fa001b58
Ricardo Ferreira <141281476+ricardoof@users.noreply.github.com>
2024-05-26 10:09:21 -0300

    correção apikey

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Middleware para servir arquivos estáticos
+app.use(express.static('public'));
+
+// Endpoint para fornecer a API Key ao cliente
+app.get('/apiKey', (req, res) => {
+    res.json({ apiKey: process.env.TMDB_API_KEY });
+});
+
+// Rota para servir o arquivo index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
+
