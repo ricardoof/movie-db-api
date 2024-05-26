c2d5340380a88dc523173da05ce7cbfe2b1692f8
Ricardo Ferreira <141281476+ricardoof@users.noreply.github.com>
2024-05-26 10:31:20 -0300

    correção deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 // Middleware para servir arquivos estáticos
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'src')));
 
 // Endpoint para fornecer a API Key ao cliente
 app.get('/apiKey', (req, res) => {
@@ -12,11 +12,10 @@ app.get('/apiKey', (req, res) => {
 
 // Rota para servir o arquivo index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
-
