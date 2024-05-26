2f529eb749e07c2a384f0d655d72e163ccf7f2ca
Ricardo Ferreira <141281476+ricardoof@users.noreply.github.com>
2024-05-26 10:44:21 -0300

    correção deploy

--- server.js
+++ server.js
@@ -3,22 +3,21 @@ const app = express();
 const path = require('path');
 
 // Middleware para servir arquivos estáticos
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname)));
 
 // Endpoint para fornecer a API Key ao cliente
-app.get('/apiKey', (_, res) => {
+app.get('/apiKey', (req, res) => {
     res.json({ apiKey: process.env.TMDB_API_KEY });
 });
 
 // Rota para servir o arquivo index.html
-app.get('*', (_, res) => {
-    const filePath = path.join(__dirname, 'src', 'index.html');
-    console.log(`Servindo arquivo: ${filePath}`);
-    res.sendFile(filePath);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
+
 
